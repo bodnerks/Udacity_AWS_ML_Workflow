31636625feee86f38597b6e2ea0918a28d9f9c3c
Kevin Bodner <bodnerks@users.noreply.github.com>
2022-06-18 19:50:22 -0700

    #Udacity AWS ML [New Exercise]  Lambda Functions, Step Functions.

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'udacity-lambda-bucket'
+PREFIX = 'musical-instrument-review'
 
 # Function below unzips the archive to the local directory. 
 
